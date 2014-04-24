bf13661dfaf882388656587b87a19c29de683580
Jennifer Hu <jhu829@gmail.com>
2014-04-24 00:10:51 -0400

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Critiqueit::Application.config.secret_key_base = 'f10ff257346573ddc7f64b6f83ced426a5ea167c4e879ee98fb6dfb9323ffb4f9fbcc10e7e278c368b4b3d1e583610e1eed9170b950919ab5bdd24aea169d2ed'
