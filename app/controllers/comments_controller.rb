8ce847eb6023fa43b11d398eff5b380ff784f301
Jennifer Hu <jhu829@gmail.com>
2014-04-30 12:17:27 -0400

    adding statuses aw yeahh

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,15 +28,13 @@ class CommentsController < ApplicationController
   def create
     @comment = @video.comments.new(comment_params)
 
-    respond_to do |format|
+
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @comment }
+        redirect_to video_path(@comment.video_id)
       else
-        format.html { render action: 'new' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        render :action => 'new'
       end
-    end
+
   end
 
   # PATCH/PUT /comments/1
