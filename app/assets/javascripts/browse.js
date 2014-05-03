098ea8bf7d0239724ff92dc63b7650cd9a967a41
Kristin Au <kristincau@Kristins-MacBook-Pro.local>
2014-05-03 16:25:13 -0400

    more additions

--- app/assets/javascripts/browse.js
+++ app/assets/javascripts/browse.js
@@ -1,6 +1,24 @@
+	function drawChart(id, yes, no, yes_to_no, no_to_yes) {
+		var rec_ctx = document.getElementById(id).getContext("2d");
+		var data = [
+			{value: yes,
+				color:"#609f45"},
+			{value : no,
+				color : "#b23e2a"},
+			{value : yes_to_no,
+				color : "#f18b40"},
+			{value : no_to_yes,
+				color : "#d1d68b"}]
+		var recvid_chart = new Chart(rec_ctx).PolarArea(data);
+		new Chart(rec_ctx).Pie(data);
+	}
+
 $(document).ready(function(){
 
+	//Side menu pull out
 	$('#nav_menu').sidr({side:'right'});
+
+	
 
 	$(".video").mouseover(function(){
 		var id = $(this).attr('id');
