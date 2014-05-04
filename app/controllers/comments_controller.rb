a416b871dd73ab2c6905baa2184cbfecb65aed8c
Jennifer Hu <jhu829@gmail.com>
2014-05-03 21:11:05 -0400

    comment javascriipt timestamp

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -73,6 +73,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:name, :comment_text, :user_id, :video_id)
+      params.require(:comment).permit(:name, :comment_text, :time_stamp, :user_id, :video_id)
     end
 end
