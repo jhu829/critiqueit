2d36675971f1167e3206eb0f1a4db2e8f797fece
Kristin Au <kristincau@Kristins-MacBook-Pro.local>
2014-05-04 19:34:28 -0400

    made fixes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Critiqueit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   get 'static_pages/home'
-  root  'static_pages#home'
+  root  'profiles#index'
 
   resources :videos do
       get :sold
