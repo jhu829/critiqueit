516181dda49c8a195d86b553d45ae8c0d947bfae
Jennifer Hu <jhu829@gmail.com>
2014-05-04 21:23:53 -0400

    comments ajax sorta, play pause video, hide/show comments etc.

--- app/assets/javascripts/comments.js.coffee
+++ app/assets/javascripts/comments.js.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$(document).ready ->
+  $("#commentform").on("ajax:success", (e, data, status, xhr) ->
+    $("#commentform").append xhr.responseText
+  ).on "ajax:error", (e, xhr, status, error) ->
+    $("#commentform").append "<p>ERROR</p>"
--- app/assets/javascripts/reviewer.js
+++ app/assets/javascripts/reviewer.js
@@ -1,125 +1,5 @@
 $(document).ready(function() {
-	var video=document.getElementById("video"); 
-	$('#cmtbtn').click(function(){
-	  		// $('#comment_list').append($('#comment').val()+ '<br>');
-	  		$('#comment_list').append('<div id="individual_comment"><table border="1" id="cmt_table"><th id="time_stamp">'+$('#comment').val().substring(1,5)+
-									'</th><th id="comment_text">'+$('#comment').val().substring(6)+'</div>')
-	  		$('#comment').val('');
-	  		video.play();
-	  	});
-
-
-	$('#changevote1').keyup(function(){
-		if(event.keyCode == 13){
-			$("#yeschange").click();
-		}
-	});
-
-	$('#changevote2').keyup(function(){
-		if(event.keyCode == 13){
-			$("#yeschange2").click();
-		}
-	});
-
-	$("#comment").keydown(function(event){ 
-		if(event.keyCode == 13){
-			$("#cmtbtn").click();
-		}
-		else if (!video.paused){
-			video.pause();
-		}
-		console.log("get here?")
-
-		var minutes = Math.floor(video.currentTime/60);
-		var seconds = Math.floor(video.currentTime-minutes*60)
-		if (seconds<10){
-			seconds='0'+seconds
-		}
-		if ($('#comment').val().length==1){
-			console.log("get here?now")
-			$('#comment').val('['+minutes+":"+seconds+'] '+$('#comment').val());
-		}
-
-	});
-
-
-
-	$(window).click(function(e) {
-		if ((42<=event.clientX<=660) && (126<=event.clientY<=200)){
-			// console.log("HELLOO")
-			// console.log(event.clientX)
-			// console.log(event.clientY)
-			// if (video.paused){
-			// 	video.play();
-			// }
-			// else{
-			// 	video.pause();
-			// }
-
-		}
-	});
-
-
-	$('#sold').click(function(){
-		$('#areyousold').addClass('hidden')
-		$('#solddisplay').removeClass('hidden')
-	});
-
-	$('#notsold').click(function(){
-		$('#areyousold').addClass('hidden')
-		$('#notsolddisplay').removeClass('hidden')
-	});
-
-
-	$('#changebtn').click(function(){
-		console.log("CLICKEDDDDD")
-
-		$('#confirmfromsold').removeClass("hidden");
-		$('#solddisplay').addClass('hidden');
-		$('#changevote1').focus()
-
-		$('#yeschange').click(function(){
-			if ($("#changevote1").val().length===0){
-				console.log("lengthbelow")
-				console.log($("#changevote1").val().length);
-				alert("You must provide a reason for changing")
-			}	
-			else{
-				console.log("alert?")
-				console.log($("#changevote1").val().length)
-				console.log($("#changevote1").val())
-				$('#confirmfromsold').addClass("hidden");
-				$('#notsolddisplay').removeClass('hidden');
-			}
-		})
-		$('#nochange').click(function(){
-			$('#confirmfromsold').addClass("hidden");
-			$('#solddisplay').removeClass('hidden');
-		})
-	});
-
-	$('#changebtn2').click(function(){
-
-		$('#confirmfromnotsold').removeClass("hidden");
-		$('#notsolddisplay').addClass('hidden');
-		$('#changevote2').focus()
-
-		$('#yeschange2').click(function(){
-			if ($("#changevote2").val().length==0){
-				alert("You must provide a reason for changing")
-			}
-			else{
-				$('#confirmfromnotsold').addClass("hidden");
-				$('#solddisplay').removeClass('hidden');
-			}
-		});
-		$('#nochange2').click(function(){
-			$('#confirmfromnotsold').addClass("hidden");
-			$('#notsolddisplay').removeClass('hidden');
-		});
-
-	});
-
+	
 	/*COMMENTS AREA*/
 
 	$('#hidecomments').click(function(){
@@ -135,17 +15,17 @@ $(document).ready(function() {
 	});
 
 	function togglePlay() {
-	    if (video.paused == true) {
+	    if (yourvideo.paused == true) {
 	    // Play the video
-	    video.play();
+	    yourvideo.play();
 	  } else {
 	    // Pause the video
-	    video.pause();
+	    yourvideo.pause();
 	  }
 	}
 
 
-	video.addEventListener("click", function() {
+	yourvideo.addEventListener("click", function() {
     	togglePlay();
   	});
 
