7ae6d8d3f5cef3e67620095b615b02946f93183b
Victoria Li <vicli@mit.edu>
2014-05-07 00:55:58 -0400

    git comiple shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Critiqueit::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
 config.serve_static_assets = true
+config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
