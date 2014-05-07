5ab6f8680933b44c22ad17f5bdfae1c03f734cb1
Victoria Li <vicli@mit.edu>
2014-05-07 01:05:28 -0400

    more precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Critiqueit::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
 config.serve_static_assets = true
 # config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
-config.assets.precompile += %w( *.css *.js )
+config.assets.precompile += %w( *.css *.js *.jpg *.png)
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
