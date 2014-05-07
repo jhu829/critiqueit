001fc38424cef8e74021ec9c23a77ecdad786c2e
Victoria Li <vicli@mit.edu>
2014-05-07 01:31:22 -0400

    prd stats

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Critiqueit::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-config.serve_static_assets = true
+# config.serve_static_assets = true
 # config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 config.assets.precompile += %w( *.css *.js *.jpg *.png)
   # Compress JavaScripts and CSS.
@@ -28,7 +28,7 @@ config.assets.precompile += %w( *.css *.js *.jpg *.png)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
