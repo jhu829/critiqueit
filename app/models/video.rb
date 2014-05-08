88161451d2808f4a3075fe6fc3314e96d1159b34
Andre Mroz <mathmonkey4@gmail.com>
2014-05-08 03:22:31 -0400

    check dat front page doh

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,7 @@ class Video < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
+	searchkick autocomplete: ['title']
 	has_attached_file :video, :styles => { :thumb => "100x100>" }
 	# validates_attachment_content_type :video, :content_type => /\Aimage\/.*\Z/
 	validates_attachment_content_type :video, :content_type => { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'video/mp4'] }
